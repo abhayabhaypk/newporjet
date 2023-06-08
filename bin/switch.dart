af2e1574cd86391443d9c473acce77eadb501045
acer <abhaycheruvadi@gmail.com>
2023-06-08 03:24:30 -0700

    commit

---
+++ bin/switch.dart
@@ -0,0 +1,16 @@
+void main(){
+  var year=2012;
+  switch(year){
+    case 2015 :
+      print(2015);
+      break;
+    case 2013 :
+      print(2013);
+      break;
+    case(2012):
+      print("year 2012");
+      break;
+    default:
+      print("invaild year");
+  }
+}
